cc524a120d8847d1ec5875b1c23c8bfc64c41282
Vladimir Gevak <vladimir.gevak@fogstream.ru>
2019-08-09 19:39:43 +1000

    change address to AWS

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,3 @@
 // export const SERVER_URL = 'http://localhost:8080';
 export const SERVER_URL =
-  'https://cors-anywhere.herokuapp.com/http://todo-app.cloudno.de';
+  'https://cors-anywhere.herokuapp.com/http://todo-server.eu-central-1.elasticbeanstalk.com';
