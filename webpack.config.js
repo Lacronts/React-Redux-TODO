ccf166815d41c745828b273d264ccbe1cb86493c
lacronts <ikuz58@gmail.com>
2019-08-09 23:55:52 +1000

    fix build path

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ const config = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/',
+    publicPath: '/React-Redux-TODO/',
   },
   devServer: {
     historyApiFallback: true,
